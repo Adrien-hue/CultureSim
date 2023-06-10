44354fe33cf485ac02e63042e7a32af9eb7b26bf
JoyTeaser <59168096+Adrien-hue@users.noreply.github.com>
2023-06-10 09:40:58 +0200

    Responsive footer

--- src/components/molecules/Footer/Footer.jsx
+++ src/components/molecules/Footer/Footer.jsx
@@ -1,35 +1,51 @@
 import "./Footer.scss";
 
 const Footer = () => {
-    return <footer className='App-footer'>
-        <div className='footerPresentation'>
-            <div className='Item footer1'>
-                <div><a href="#">About</a></div>
-                <div><a href="#">Business</a></div>
-                <div><a href="#">Affilates</a></div>
-                <div><a href="#">Careers</a></div>
-                <div><a href="#">News</a></div>
-            </div>
+    return <footer className="app-footer">
+        <section className="footer-sections">
+            <section>
+                <menu>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Business</a></li>
+                        <li><a href="#">Affilates</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </menu>
+            </section>
 
-            <div className='Item footer2'>
-                <div><a href="#">Legal issues</a></div>
-                <div><a href="#">Term of use</a></div>
-                <div><a href="#">Privacy</a></div>
-                <div><a href="#">Copyright</a></div>
-                <div><a href="#">Accessability</a></div>
-                <div><a href="#">Other</a></div>
-            </div>
+            <section>
+                <menu>
+                    <ul>
+                        <li><a href="#">Legal issues</a></li>
+                        <li><a href="#">Term of use</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Copyright</a></li>
+                        <li><a href="#">Accessability</a></li>
+                        <li><a href="#">Other</a></li>
+                    </ul>
+                </menu>
+            </section>
 
-            <div className='Item footer3'>
-                <div><a href="#">Contact us</a></div>
-                <div><a href="#">Teaching</a></div>
-                <div><a href="#">Help centre</a></div>
-                <div><a href="#">Editorial board</a></div>
-                <div><a href="#">Reviews</a></div>
-                <div><a href="#">Manual</a></div>
-                <div><a href="#">Partners</a></div>
-            </div>
-        </div>
+            <section>
+                <menu>
+                    <ul>
+                        <li><a href="#">Contact us</a></li>
+                        <li><a href="#">Teaching</a></li>
+                        <li><a href="#">Help centre</a></li>
+                        <li><a href="#">Editorial board</a></li>
+                        <li><a href="#">Reviews</a></li>
+                        <li><a href="#">Manual</a></li>
+                        <li><a href="#">Partners</a></li>
+                    </ul>
+                </menu>
+            </section>
+        </section>
+
+        <section className="footer-bottom">
+            @Developed at ESIEE Paris
+        </section>
   </footer>
 }
 
