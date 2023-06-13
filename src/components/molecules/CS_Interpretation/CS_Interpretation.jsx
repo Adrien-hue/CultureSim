8a497701e6f64229c8597d0e73677cfaf564336f
JoyTeaser <59168096+Adrien-hue@users.noreply.github.com>
2023-06-13 11:28:39 +0200

    Clear CS_interpretation

--- src/components/molecules/CS_Interpretation/CS_Interpretation.jsx
+++ src/components/molecules/CS_Interpretation/CS_Interpretation.jsx
@@ -3,17 +3,6 @@ import "./CS_Interpretation.scss";
 import { PTitle } from "../../atoms";
 
 const CS_Interpretation = ({content, title, isTrue, btnFunction, ...props}) => {
-    const handleClick = () => {
-        let plouf = `this answer is `;
-
-        if(isTrue){
-            plouf += `correct`;
-        } else {
-            plouf += `incorrect`;
-        }
-        alert(plouf);
-    }
-
     return <button className="CS_interpretation" onClick={btnFunction}>
         <PTitle title={title} content={content} />
     </button>
