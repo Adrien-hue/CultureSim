343927c23a09f5727fe087fec98985325d664273
JoyTeaser <59168096+Adrien-hue@users.noreply.github.com>
2023-06-16 00:04:40 +0200

    design with class

--- src/components/molecules/Datatable/Datatable.jsx
+++ src/components/molecules/Datatable/Datatable.jsx
@@ -1,55 +1,56 @@
 import "./Datatable.scss";
-import React from 'react';
-import  { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
+function Datatable({ json, ...props }) {
+	let columns = json.columns;
+	let data = json.data;
 
-function Datatable({ json, ...props}) {
+	const [users] = useState(data);
 
-  let columns = json.columns;
-  let data = json.data;
+	return (
+		<table className="datatable">
+			<thead className="datatable-thead">
+				<tr className="thead-row">
+					{Object.keys(columns).map((key) => {
+						if (columns[key].display) {
+							return (
+								<th key={key} className="thead-cell">
+									{columns[key].name}
+								</th>
+							);
+						}
+					})}
+					<th className="thead-cell"> Action </th>
+				</tr>
+			</thead>
+			<tbody className="datatable-tbody">
+				{users.map((user) => (
+					<tr className="tbody-row" key={user.id}>
+						{Object.keys(columns).map((key) => {
+							if (columns[key].display) {
+								return (
+									<td className="tbody-cell" key={key}>
+										{user[key]}
+									</td>
+								);
+							}
+						})}
 
-  
-  const [users] = useState(data);
+						<td className="tbody-cell tbody-action_cell">
+							<button className="button modify-button">
+								Modifier
+							</button>
 
-    return (
-     
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(columns).map((key) =>{
-              if(columns[key].display){
-                return <th key={key}>{columns[key].name}</th>
-              }
-            })}
-            <th> Action </th>
-            
-          </tr>
-        </thead>
-        <tbody>  
-          {users.map((user) => (
-            <tr key={user.id}>
-             {Object.keys(columns).map((key) => {
-              if (columns[key].display) {
-                return <td key={key}>{user[key]}</td>;
-                }
-                
-              })}
-              
-              <td className="lmj-Datatable-button-td">
-              
-                 <button  className="lmj-Datatable-button-modify" >Modifier</button>
-                 
-                 <button  className="lmj-Datatable-button-suppr">Supprimer</button> 
-                    
-             </td>
-
-            </tr>
-          ))}
-        </tbody>
-      
-    </table>
-    
-  );
+							<button className="button delete-button">
+								Supprimer
+							</button>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 }
 
 export default Datatable;
