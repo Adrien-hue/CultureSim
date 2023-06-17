0b95210db6309a733c0ed7a182dc7432f9db042c
nadine <ndnbelinga@gmail.com>
2023-06-17 14:46:34 +0200

    Revert "responsive card"
    This reverts commit 389b290c005bb4c5ad1e14d95f7cebbdf54a47d1.

--- src/components/atoms/CountryCard/CountryCard.jsx
+++ src/components/atoms/CountryCard/CountryCard.jsx
@@ -2,14 +2,12 @@ import "./CountryCard.scss";
 
 const CountryCard = ({name, image, ...props}) => {
     return <div className="countryCard">
-        <div className="countryCard-container">
-            <div className="countryCard-title">{name}</div>
-            <img 
-                src={require(`../../../assets/` + image)} 
-                alt={`Image of ` + name} 
-                className="countryCard-image"
-            />
-        </div>
+        <div className="countryCard-title">{name}</div>
+        <img 
+            src={require(`../../../assets/` + image)} 
+            alt={`Image of ` + name} 
+            className="countryCard-image"
+        />
     </div>
 }
 
