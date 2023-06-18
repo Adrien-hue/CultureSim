4a0d260113c9fc8f5382eeb2208d3611acdba43e
nadine <ndnbelinga@gmail.com>
2023-06-18 12:47:07 +0200

    Responsive countryCard

--- src/components/atoms/CountryCard/CountryCard.jsx
+++ src/components/atoms/CountryCard/CountryCard.jsx
@@ -1,13 +1,20 @@
 import "./CountryCard.scss";
 
 const CountryCard = ({name, image, ...props}) => {
-    return <div className="countryCard">
-        <div className="countryCard-title">{name}</div>
-        <img 
-            src={require(`../../../assets/` + image)} 
-            alt={`Image of ` + name} 
-            className="countryCard-image"
-        />
+    return <div className="countryCards">
+        <div className="countryCard">
+            <div className="countryCard-container">
+                <img 
+                    src={require(`../../../assets/` + image)} 
+                    alt={`Image of ` + name} 
+                    className="countryCard-image"
+                />
+            </div>
+                <div className="countryCard-details">
+                    <h3>{name}</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis modi fuga veniam est dolor praesentium aperiam commodi laboriosam doloremque cupiditate, libero, sapiente nulla fugit beatae.</p>
+                </div>
+        </div>
     </div>
 }
 
