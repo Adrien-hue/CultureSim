6691d082ecb06e60e93670468e1b991efe513ab3
JoyTeaser <59168096+Adrien-hue@users.noreply.github.com>
2023-06-18 18:59:36 +0200

    CountryCard layout

--- src/components/atoms/CountryCard/CountryCard.jsx
+++ src/components/atoms/CountryCard/CountryCard.jsx
@@ -1,21 +1,20 @@
 import "./CountryCard.scss";
 
-const CountryCard = ({name, image, ...props}) => {
-    return <div className="countryCards">
-        <div className="countryCard">
-            <div className="countryCard-container">
-                <img 
-                    src={require(`../../../assets/` + image)} 
-                    alt={`Image of ` + name} 
-                    className="countryCard-image"
-                />
-            </div>
-                <div className="countryCard-details">
-                    <h3>{name}</h3>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis modi fuga veniam est dolor praesentium aperiam commodi laboriosam doloremque cupiditate, libero, sapiente nulla fugit beatae.</p>
-                </div>
-        </div>
-    </div>
-}
+const CountryCard = ({ name, image, ...props }) => {
+	return (
+		<div className="countryCard">
+			<div className="countryCard-container">
+				<img
+					src={require(`../../../assets/` + image)}
+					alt={name}
+					className="countryCard-image"
+				/>
+			</div>
+			<div className="countryCard-details">
+				<h3>{name}</h3>
+			</div>
+		</div>
+	);
+};
 
 export default CountryCard;
